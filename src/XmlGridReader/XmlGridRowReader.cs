f54d9dac4e7ec9b691d49350cf582ccbfda5d13d
bdlane <brendan@bdlane.com>
2020-12-04 18:42:46 +0000

    Simlify ReadRow check, update comments

--- src/XmlGridReader/XmlGridRowReader.cs
+++ src/XmlGridReader/XmlGridRowReader.cs
@@ -11,18 +11,16 @@ namespace XmlGridReader
             new XmlReaderSettings { IgnoreWhitespace = true };
 
         private readonly XmlReader reader;
+        private int currentColumnIndex = 0;
 
         public XmlGridRowReader(string xml)
         {
             reader = XmlReader.Create(new StringReader(xml), settings);
         }
 
-        // Consider return type...
-        public List<string> Columns { get; private set; } =
-            new List<string>();
+        public List<string> Columns { get; private set; } = new List<string>();
 
-        public List<string> Values { get; private set; } =
-            new List<string>();
+        public List<string> Values { get; private set; } = new List<string>();
 
         /// <summary>
         /// Moves the reader to the grid, positioned at the first row
@@ -41,26 +39,29 @@ namespace XmlGridReader
             return reader.NodeType == XmlNodeType.Element;
         }
 
+        /// <summary>
+        /// Reads the current row and advances the reader to the start of the
+        /// next row, or to the end of the root element.
+        /// </summary>
+        /// <returns><see cref="true"/> if the row was read, <see cref="false"/> otherwise.</returns>
         public bool ReadRow()
         {
-            // Assume we either start on
-            //  <Row>, OR </Data>
+            // We should either be on a <Row>, </Root> or NodeType.None
 
+            // Reset the row
             // Is this performant?
-            current = 0;
+            // We currently only need to get the column names on the first row
+            currentColumnIndex = 0;
             Columns.Clear();
             Values.Clear();
 
-            if (reader.NodeType == XmlNodeType.EndElement
-                || reader.NodeType == XmlNodeType.None)
+            if (reader.NodeType != XmlNodeType.Element)
             {
                 return false;
             }
 
-            // Really, we only need the column names
-            // on the first row... for now?
-
-            // Advances from <Row> to <Col>, OR
+            // Advances from <Row> to <Col>
+            // TODO: check this is <Col>, and not an empty row -> exception
             reader.Read();
 
             while (reader.NodeType != XmlNodeType.EndElement)
@@ -76,12 +77,11 @@ namespace XmlGridReader
             return true;
         }
 
-        int current = 0;
-        // Just for the moment...
-        public string ReadColumnValue()
-        {
-            return Values[current++];
-        }
+        /// <summary>
+        /// Reads the current column value as a string, and advances the
+        /// column index. Call this method after calling <see cref="ReadRow"/>.
+        /// </summary>
+        public string ReadColumnValue() => Values[currentColumnIndex++];
 
         public void Dispose() => reader.Dispose();
     }
